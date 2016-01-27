09a34fc7d02168cbb473c817bc1dd314a42a86fa
aaronmarkle <aaronkmarkle@gmail.com>
2016-01-27 13:20:20 -0800

    basic form template being laid out with angulary-formly #1

--- default.js
+++ default.js
@@ -2,8 +2,39 @@ var app = angular.module('formlyApp', ['formly', 'formlyBootstrap']);
 
 app.controller('MainController', function(beach) {
   var vm = this;
-  vm.rental = {};
-  vm.rentalFields = []
+  vm.instructor = {};
+  vm.instructorFields = [
+    {
+      key: 'first_name',
+      type: 'input',
+      templateOptions: {
+        type: 'text',
+        label: 'First Name',
+        placeholder: 'Enter your first name',
+        required: true
+      }
+    },
+    {
+      key: 'last_name',
+      type: 'input',
+      templateOptions: {
+        type: 'text',
+        label: 'Last Name',
+        placeholder: 'Enter your last name',
+        required: true
+      }
+    },
+    {
+      key: 'email',
+      type: 'input',
+      templateOptions: {
+        type: 'email',
+        label: 'Email address',
+        placeholder: 'Enter email',
+        required: true
+      }
+    },
+  ];
 });
 
 app.factory('beach', function() {
