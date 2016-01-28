530ab5ea76b1215019a44b49264f81288fc27684
aaronmarkle <aaronkmarkle@gmail.com>
2016-01-27 22:58:05 -0800

    server now checks db for email to see if it is already in use before creating new instructor from form #1

--- app.js
+++ app.js
@@ -34,16 +34,25 @@ app.get('/signup', function(req, res) {
 });
 
 app.post('/signup-submit', jsonParser, function(req, res) {
-  var newInstructor = new Instructor({
-    email: req.body.email,
-    password: req.body.password,
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    phone: req.body.phone,
-    beaches: req.body.beaches
-  });
-  newInstructor.save(function(err) {
-    if (err) throw err;
+  Instructor.findOne({'email': req.body.email}, function(err, instructor) {
+    if (err) {
+      console.log('error has occurred searching for instructor');
+    }
+    if (instructor) {
+      console.log('that email address is already in use');
+    } else {
+    var newInstructor = new Instructor({
+      email: req.body.email,
+      password: req.body.password,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      phone: req.body.phone,
+      beaches: req.body.beaches
+    });
+    newInstructor.save(function(err) {
+      if (err) throw err;
+    });  
+    }
   });
   res.json(req.body);
 });
