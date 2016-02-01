f5148132788d13487b7d75d4e9469fea4e666e3a
aaronmarkle <aaronkmarkle@gmail.com>
2016-01-31 16:40:49 -0800

    added some additional styling

--- public/default.js
+++ public/default.js
@@ -3,11 +3,12 @@ app = angular.module('surfSearch', ['beachesModule']);
 app.controller('SearchController', function(beaches, $http) {
   var vm = this;
   vm.options = beaches.getBeaches();
-  vm.selectedBeach = {};
   vm.search = function() {
-    $http.post('search', vm.selectedBeach)
-      .success(function(data) {
-        vm.results = data;
-      })
+    if (vm.selectedBeach) {
+      $http.post('search', vm.selectedBeach)
+        .success(function(data) {
+          vm.results = data;
+        })
+    }
   }
 });
